cef0080107ac7883e9848bee4bd7d154a3b0e67e
hanojiang <whj773768856@gmail.com>
2018-10-28 16:11:25 +0800

    mwxfunction rplot findlines

---
+++ 程序语言/c++/code/cmex.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include "mex.h"
+#include "matrix.h"
+using namespace std;
+
+void findlines(int8_t * data, int8_t * lines, int len)
+{
+    
+    for (int i = 0; i < len; i++) {
+        lines[i] = 0;
+    }
+
+    int left = 0, right = 0;
+    while (left<len) {
+        int len_sub = 0;
+        while (right<len && data[left]==1 && data[right]==1) {
+            len_sub += 1;
+            right += 1;
+        }
+        if (len_sub>0) {
+            lines[len_sub - 1] = lines[len_sub - 1] + 1;
+        }
+        left = right + 1;
+        right = left;
+    }
+}
+
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    int len = mxGetM(prhs[0]);
+
+    int8_t *data = (int8_t *)mxGetData(prhs[0]);
+    plhs[0]  = mxCreateNumericArray (1, len, mxINT8_CLASS, mxREAL);
+
+    int8_t *lines;
+    lines=(int8_t *)mxGetData(plhs[0]);
+
+    findlines(data, lines, len);
+    
+
+    // double *data;
+    // int8_t * lines;
+    // data = mxGetPr(prhs[0]);
+    // plhs[0]  = mxCreateNumericArray (1, length, mxINT8_CLASS, mxREAL);
+    // lines=mxGetPr(plhs[0]);
+    // int8_t data_int[length];
+    // for (int i = 0; i < length; i++) {
+    //     data_int[i] = int8_t(data[i]);
+    // }
+    // vector<int8_t> vec_data(data_int, data_int + length);
+    // findlines(vec_data, lines);
+}
+
+// int main(int argc, char const *argv[]) {
+    
+//     // int x[]={0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1 };
+// // % y=[1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+//     int x[] = {0, 1, 0, 1, 0, 1};
+//     const int length = 6;
+//     vector<int> data(x, x + length);
+//     int lines[length];
+//     findlines(data, lines);
+//     for (int i = 0; i < length; i++) {
+//         cout << lines[i] << endl;
+//     }
+//     return 0;
+// }
