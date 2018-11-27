f4c8bd96ce3e489ae65f42844f0c278f935ef53c
hanojiang <whj773768856@gmail.com>
2018-11-27 09:45:52 +0800

    cpp test

--- 程序语言/c++/essential/chap3/file_io_test.cpp
+++ 程序语言/c++/essential/chap3/file_io_test.cpp
@@ -4,18 +4,27 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-    string s;
-    vector<string> vs;
-    while(cin>>s)
-        vs.push_back(s);
-    for (auto i = vs.begin(); i != vs.end();++i)
-    {
-        cout << *i << endl;
+    // string s;
+    // vector<string> vs;
+    // while(cin>>s)
+    //     vs.push_back(s);
+    // for (auto i = vs.begin(); i != vs.end();++i)
+    // {
+    //     cout << *i << endl;
+    // }
+
+    // for(auto j : vs)
+    // {
+    //     cout << j << endl;
+    // }
+
+    vector<int> a(5,1);
+    int len = a.size();
+    int b[len];
+    for (int i = 0; i < len; i++) {
+        b[i]=i;
+        cout << b[i] << endl;
     }
 
-    for(auto j : vs)
-    {
-        cout << j << endl;
-    }
     return 0;
 }
---
+++ 程序语言/c++/essential/chap3/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+string s;
+    // vector<string> vs;
+    // while(cin>>s)
+    //     vs.push_back(s);
+    // for (auto i = vs.begin(); i != vs.end();++i)
+    // {
+    //     cout << *i << endl;
+    // }
+
+    // for(auto j : vs)
+    // {
+    //     cout << j << endl;
+    // }
